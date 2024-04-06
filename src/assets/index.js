e0d796196610087f069c30da3fdf148c3a41fa56
Owen Zhang <67778083+owenz0523@users.noreply.github.com>
2024-04-06 16:53:36 -0400

    Initial commit

---
+++ src/assets/index.js
@@ -0,0 +1,89 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+import python from "./tech/python.png";
+import opencv from "./tech/opencv.png";
+import tensorflow from "./tech/tensorflow.png";
+import cpp from "./tech/c++.png";
+import aws from "./tech/amazon.png";
+import sql from "./tech/sql.png";
+import postman from "./tech/postman.svg";
+
+import pillthought from "./company/pillthought.png";
+import reviewkit from "./company/reviewkit.svg";
+import apple from "./company/apple.png";
+import openswiki from "./company/openswiki.png";
+import martinrea from "./company/martinrea.png"
+
+import pharmfill from "./pharmfill.png";
+import pipaws from "./pipaws.png";
+import beacons from "./beacons.png";
+import gymmanager from "./gymmanager.png";
+import stockbot from "./stockbot.png";
+import portfolio from "./portfolio.png";
+
+import waterloo from "./waterloo.png";
+import fx from "./fx.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  pillthought,
+  reviewkit,
+  apple,
+  openswiki,
+  martinrea,
+  pharmfill,
+  pipaws,
+  beacons,
+  gymmanager,
+  stockbot,
+  python,
+  opencv,
+  tensorflow,
+  cpp,
+  aws,
+  sql,
+  postman,
+  waterloo,
+  fx,
+  portfolio,
+};
---
+++ src/components/About.jsx
@@ -0,0 +1,109 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from '../styles';
+import { services } from '../constants';
+import { fadeIn, textVariant } from '../utils/motion';
+import { SectionWrapper } from '../hoc';
+import Typewriter from "typewriter-effect";
+
+const ServiceCard = ({index, title, icon}) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card">
+          <div
+            onCompositionStart={{
+              max: 45,
+              scale: 1,
+              speed: 450,
+            }}
+            className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex
+            justify-evenly items-center flex-col">
+              <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+              <h3 className="text-white text-[20p] font-bold text-center">{title}</h3>
+          </div>
+      </motion.div>
+    </Tilt>
+  )
+}
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[20px] max-w-3xl leading-[30px]"
+      >
+        I am a{" "}
+        <span className="text-[#99EDC3] inline align-middle">
+        <Typewriter
+          options={{
+            strings: ["Full Stack Developer", "App Developer", "AI Developer", "Programmer"],
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 50,
+            delay: 75,
+            color: "#915EFF",
+          }}
+        />
+        </span>
+      </motion.p>
+              <ul className="list-none hidden sm:flex flex-row gap-10 my-10 justify-center items-center">
+                <li
+                  className="flex items-center border border-white text-secondary
+                justify-center rounded-full cursor-pointer px-5 py-2.5 text-base font-medium 
+                transition duration-600 ease-in-out no-underline hover:bg-[#915eff] hover:text-white"
+                >
+                  <a
+                    href="https://drive.google.com/file/d/1XA1lRwqfaeorLcKchIP5WorVWZlgYOFI/view?usp=sharing"
+                    target="_blank"
+                    className="text-white text-[18px] font-medium cursor-pointer"
+                  >
+                    Resume
+                  </a>
+                </li>
+                <li
+                  className="flex items-center border border-white text-secondary
+                justify-center rounded-full cursor-pointer px-5 py-2.5 text-base font-medium 
+                transition duration-600 ease-in-out no-underline hover:bg-[#915eff] hover:text-white"
+                >
+                  <a
+                    href="https://github.com/owenz0523"
+                    target="_blank"
+                    className="text-white text-[18px] font-medium cursor-pointer"
+                  >
+                    Github
+                  </a>
+                </li>
+                <li
+                  className="flex items-center border border-white text-secondary
+                justify-center rounded-full cursor-pointer px-5 py-2.5 text-base font-medium 
+                transition duration-600 ease-in-out no-underline hover:bg-[#915eff] hover:text-white"
+                >
+                  <a
+                    href="https://www.linkedin.com/in/owenzhang052/"
+                    target="_blank"
+                    className="text-white text-[18px] font-medium cursor-pointer"
+                  >
+                    LinkedIn
+                  </a>
+                </li>
+              </ul>
+
+              <div className="mt-20 flex flex-wrap gap-10">
+                {services.map((service, index) => (
+                  <ServiceCard key={service.title} index={index} {...service}/>
+                ))}
+              </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about")
