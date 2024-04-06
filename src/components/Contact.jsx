daa86b71dce6014932ac7f382310686dbf8e4085
Owen Zhang <67778083+owenz0523@users.noreply.github.com>
2024-04-06 17:01:03 -0400

    fixed slide in for planet

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -114,7 +114,7 @@ const Contact = () => {
         </form>
       </motion.div>
       <motion.div
-        variants={slideIn('left', 'tween', 0.2, 1)}
+        variants={slideIn('right', 'tween', 0.2, 1)}
         className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
       >
         <EarthCanvas />
